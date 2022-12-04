395b8bea97de9c8d4b97930aedc8da9eae4f621c
Felipe Madureira de Souza <Felippefms3@gmail.com>
2022-12-04 01:10:06 -0300

    Update
    Adicionadas rotas para navegação nos menus

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -1,6 +1,12 @@
+import { FriendsWrapper } from "../styles/Friends";
+import Menu from '../components/Menu';
+
 function Friends(){
     return(
-        <></>
+        <FriendsWrapper>
+        <h1>Lista de Amigos</h1>
+        <Menu></Menu>
+        </FriendsWrapper>
     );
 }
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,13 @@
+import { ProfileWrapper } from "../styles/Profile";
+import Menu from '../components/Menu';
 
 function Profile(){
-
+    return(
+        <ProfileWrapper>
+        <h1>Perfil</h1>
+        <Menu></Menu>
+        </ProfileWrapper>
+    );
 }
 
 export default Profile;
